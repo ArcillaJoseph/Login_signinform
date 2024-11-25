3606d569da59b0b7bab0a03e58a8c826f9bdb9b0
Ace Raphael Erive <joseph.arcilla@goldenlink.ph>
2024-11-25 11:26:46 +0800

    Signin_loginForm

---
+++ script.js
@@ -0,0 +1,40 @@
+const coords = { x: 0, y: 0};
+const circles = document.querySelectorAll(".circle");
+
+circles.forEach(function(circle, index){
+    circle.x = 0;
+    circle.y = 0;
+});
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX;
+  coords.y = e.clientY;
+
+circles.forEach(function(circle, index){
+  circle.style.left = coords.x - 12 + "px";
+  circle.styleMap.top = coords.y - 12 + "px";
+  console.log(coords);
+});
+
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    if (!email || !password) {
+        alert("Please fill in both email and password.");
+    } else {
+        alert("Login Successful!");
+    }
+});
+
+document.querySelector(".forgot-password").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    alert("Please check your email to reset your password.");
+});
+
+document.querySelector(".signup-prompt a").addEventListener("click", function(event) {
+    event.preventDefault(); 
+    alert("Redirecting to Sign Up page...");
+
+});
