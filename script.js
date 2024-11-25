fbe9de421faed1cbbc58957b341a31c795e6cd0a
Ace Raphael Erive <joseph.arcilla@goldenlink.ph>
2024-11-25 11:51:55 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const coords = { x: 0, y: 0};
 const circles = document.querySelectorAll(".circle");
 
-circles.forEach(function(circle, index){
+circles.forEach(function(circle) {
     circle.x = 0;
     circle.y = 0;
 });
+
 window.addEventListener("mousemove", function(e){
   coords.x = e.clientX;
   coords.y = e.clientY;
 
-circles.forEach(function(circle, index){
+  animateCircles(){
+
+});
+
+function animatedCircles() {
+
+    let x = coords.x;
+    let y = coords.y;
+
+ circles.forEach(function(circle, index) {
   circle.style.left = coords.x - 12 + "px";
   circle.styleMap.top = coords.y - 12 + "px";
+  circle.x = x;
+  circle.y = y;
+  
+  const nextCircle = circles[index + 1] || circles[0];
+  x
   console.log(coords);
-});
+ });
+
+}
+
+
 
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault(); 
